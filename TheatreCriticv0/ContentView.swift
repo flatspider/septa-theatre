2206ce9db21f5ac29d44436fcc57c29ba6e1784c
Conor McManamon <c.s.mcmanamon@gmail.com>
2024-07-29 14:56:07 -0400

    Initial Commit

---
+++ TheatreCriticv0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TheatreCriticv0
+//
+//  Created by Conor McManamon on 7/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Greenville!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TheatreCriticv0/TheatreCriticv0App.swift
@@ -0,0 +1,17 @@
+//
+//  TheatreCriticv0App.swift
+//  TheatreCriticv0
+//
+//  Created by Conor McManamon on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct TheatreCriticv0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
